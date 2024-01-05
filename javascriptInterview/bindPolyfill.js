774a265f79712e763a2ab2a5de3941f173c6fee9
Parth Sarthy <parthmishra0219@gmail.com>
2024-01-05 09:29:35 +0000

    polyfill for bind method

--- javascriptInterview/bindPolyfill.js
+++ javascriptInterview/bindPolyfill.js
@@ -1,11 +1,24 @@
-let name = {
+let name= {
     firstname: "Parth",
     lastname: "Sarthy"
 }
 
-let printName = function (){
-    console.log(this.firstname+ " " + this.lastname);
+let printName = function (hometown,state){
+    console.log(this.firstname+ " " + this.lastname + " ," + hometown+" ," + state);
 }
 
-let printMyName = printName.bind(name);
-printMyName();
+let printMyName = printName.bind(name,"gaya");
+printMyName("Bihar");
+
+
+Function.prototype.mybind = function(...args){
+    let obj = this,
+    params =args.slice(1);
+    return function(...args2){
+       obj.apply(args[0],[ ...params,...args2]);
+    }
+
+}
+
+let printMyName2 = printName.mybind(name, "gaya");
+printMyName2("Bihar");
